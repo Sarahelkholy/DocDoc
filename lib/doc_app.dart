eec895919b75822638da9c7964969c95d35d99d7
sarah elkholy <saraelkholyarmy03@gmail.com>
2025-10-07 12:31:21 +0300

    - added assets/images and assets/svgs - added flutter_svg and flutter_screenutil - create client app - create navigation extention - create app router - create colorsManager - create TextStyels file - finished onBoarding UI (logo&name - doctor Image and text - get started button)

---
+++ lib/doc_app.dart
@@ -0,0 +1,27 @@
+import 'package:docdoc/core/routing/app_router.dart';
+import 'package:docdoc/core/routing/routes.dart';
+import 'package:docdoc/core/theming/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class DocApp extends StatelessWidget {
+  final AppRouter appRouter;
+  const DocApp({super.key, required this.appRouter});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      builder: (_, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Doc app',
+          theme: ThemeData(primaryColor: ColorsManager.mainBlue),
+          onGenerateRoute: appRouter.generateRoute,
+          initialRoute: Routes.onBoardingScreen,
+        );
+      },
+    );
+  }
+}
